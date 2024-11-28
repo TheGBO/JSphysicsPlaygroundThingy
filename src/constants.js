70f1e921261c4dfc68667017eb20cbc6746e8cad
TheGBO <gabrielthejota@gmail.com>
2024-11-27 21:23:29 -0300

    adds damping and phyisics simulation

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+import { Vector2 } from "./vector2.js";
+
+export const meter = 250;
+export const gravity = new Vector2(0, meter * 40);
--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ var canvas = document.getElementById("main-canvas");
 var ctx = canvas.getContext('2d');
 var canvasVec2;
 
-//250px = 1 meter in this simulation because i want to
-const meter = 250;
 
 function refreshCanvasSize(){
     canvasVec2 = new Vector2(document.documentElement.clientWidth, document.documentElement.clientHeight);
@@ -27,20 +25,47 @@ var elapsedTime;
 var deltaTime;
 var lastFrameTime = Date.now();
 
-let ball1 = new BallEntity(new Vector2(0,0), "#00eecc", 20, new Vector2(1110,0), new Vector2(0, 1980.8), 10)
+let ball1 = new BallEntity(new Vector2(20, 0), "#00eecc", 20, new Vector2(0, 0), new Vector2(0, 0), 10, canvasVec2, 0.65)
 console.log(ball1);
+let f = 5000;
 
+document.addEventListener('keypress', (e) =>{
+    console.log(e.key);
+    
+    switch (e.key) {
+        case 'a':
+            ball1.addImpulse(new Vector2(-f, 0));
+            break;
+        case 'd':
+            ball1.addImpulse(new Vector2(f, 0));
+            break;
+        case 'w':
+            ball1.addImpulse(new Vector2(0, -f));
+            break;   
+        case 's':
+            ball1.addImpulse(new Vector2(0, f));
+            break;             
+        default:
+            break;
+    }
+    console.log(1);
+    
+});
 function mainLoop(){
-    requestAnimationFrame(mainLoop);
     ctx.clearRect(0,0, canvasVec2.x, canvasVec2.y);
     let currentTime = Date.now();
     elapsedTime = currentTime-startTime;
     deltaTime = (currentTime - lastFrameTime)/1000;
     lastFrameTime = currentTime;
     
+    //physics
+    
+
     ball1.update(deltaTime);
     ball1.render(ctx);
 
 }
 
-mainLoop();
+setInterval(() => {
+    mainLoop();
+}, 1000/60);
